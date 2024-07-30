d2228fc95e9a407ceb58529816bf25ae7792acba
nextorsalinas <168105778+nextorsalinas@users.noreply.github.com>
2024-07-30 11:23:53 -0600

    Se Crea un scrip  en Pyton
    Para cambiar el nombre de multiples archivos

---
+++ rename_files.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+def rename_files(directory):
+    for filename in os.listdir(directory):
+        # Check if it's a file (not a directory)
+        if os.path.isfile(os.path.join(directory, filename)):
+            # Use a regular expression to remove everything before the first underscore including the underscore
+            new_name = re.sub(r'^\d+_', '', filename)
+            os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
+            print(f'Renamed: {filename} -> {new_name}')
+
+if __name__ == "__main__":
+    directory = r'C:\Users\nsalinas\Documents\numeros'  # Reemplaza con la ruta al directorio con tus archivos
+    rename_files(directory)
+
+
